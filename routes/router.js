409a3d446ddce1d0184815f0f4b456fed978ba5f
maggieslei <leimaggiese@gmail.com>
2024-01-30 00:03:30 -0500

    chore: register, login forms

--- routes/router.js
+++ routes/router.js
@@ -12,8 +12,19 @@ router.get('/about', (req, res) => {
 router.get('/register', (req, res) => {
   res.render('register', { pagename: 'Register' });
 });
+
+router.post('/register', (req, res) => {
+  console.log('Registering');
+  res.render('home', { pagename: 'Home' });
+});
+
 router.get('/login', (req, res) => {
   res.render('login', { pagename: 'Login' });
+});
+
+router.post('/login', (req, res) => {
+  console.log('logging in');
+  res.render('about', { pagename: 'About' });
 });
 
 module.exports = router;
